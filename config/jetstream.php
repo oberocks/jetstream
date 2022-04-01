dec029eea98615672bbac4d54cd8d2e6cde21ec6
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-03-31 23:15:02 -0400

    Adding view file paths to jetstream config

--- config/jetstream.php
+++ config/jetstream.php
@@ -7,4 +7,20 @@ return [
     'middleware' => ['web'],
     'features' => [Features::accountDeletion()],
     'profile_photo_disk' => 'public',
+    'view' => [
+        'api' => [
+            'index' => 'API/Index',
+        ],
+        'features' => [
+            'privacy' => 'PrivacyPolicy',
+            'terms' => 'TermsOfService',
+        ],
+        'profile' => [
+            'show' => 'Profile/Show',
+        ],
+        'teams' => [
+            'create' => 'Teams/Create',
+            'show' => 'Teams/Show',
+        ],
+    ],
 ];
