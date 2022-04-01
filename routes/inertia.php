a75202cb309080037a1b7df4ec09190f8e5c7311
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-04-01 00:12:12 -0400

    added config uri opts to routes for client views

--- routes/inertia.php
+++ routes/inertia.php
@@ -16,8 +16,8 @@ use Laravel\Jetstream\Jetstream;
 
 Route::group(['middleware' => config('jetstream.middleware', ['web'])], function () {
     if (Jetstream::hasTermsAndPrivacyPolicyFeature()) {
-        Route::get('/terms-of-service', [TermsOfServiceController::class, 'show'])->name('terms.show');
-        Route::get('/privacy-policy', [PrivacyPolicyController::class, 'show'])->name('policy.show');
+        Route::get(config('jetstream.uri.features.terms', '/terms-of-service'), [TermsOfServiceController::class, 'show'])->name('terms.show');
+        Route::get(config('jetstream.uri.features.privacy', '/privacy-policy'), [PrivacyPolicyController::class, 'show'])->name('policy.show');
     }
 
     $authMiddleware = config('jetstream.guard')
@@ -30,7 +30,7 @@ Route::group(['middleware' => config('jetstream.middleware', ['web'])], function
 
     Route::group(['middleware' => array_values(array_filter([$authMiddleware, $authSessionMiddleware, 'verified']))], function () {
         // User & Profile...
-        Route::get('/user/profile', [UserProfileController::class, 'show'])
+        Route::get(config('jetstream.uri.profile.show', '/user/profile'), [UserProfileController::class, 'show'])
                     ->name('profile.show');
 
         Route::delete('/user/other-browser-sessions', [OtherBrowserSessionsController::class, 'destroy'])
@@ -46,7 +46,7 @@ Route::group(['middleware' => config('jetstream.middleware', ['web'])], function
 
         // API...
         if (Jetstream::hasApiFeatures()) {
-            Route::get('/user/api-tokens', [ApiTokenController::class, 'index'])->name('api-tokens.index');
+            Route::get(config('jetstream.uri.api.index', '/user/api-tokens'), [ApiTokenController::class, 'index'])->name('api-tokens.index');
             Route::post('/user/api-tokens', [ApiTokenController::class, 'store'])->name('api-tokens.store');
             Route::put('/user/api-tokens/{token}', [ApiTokenController::class, 'update'])->name('api-tokens.update');
             Route::delete('/user/api-tokens/{token}', [ApiTokenController::class, 'destroy'])->name('api-tokens.destroy');
@@ -54,9 +54,9 @@ Route::group(['middleware' => config('jetstream.middleware', ['web'])], function
 
         // Teams...
         if (Jetstream::hasTeamFeatures()) {
-            Route::get('/teams/create', [TeamController::class, 'create'])->name('teams.create');
+            Route::get(config('jetstream.uri.teams.create', '/teams/create'), [TeamController::class, 'create'])->name('teams.create');
             Route::post('/teams', [TeamController::class, 'store'])->name('teams.store');
-            Route::get('/teams/{team}', [TeamController::class, 'show'])->name('teams.show');
+            Route::get(config('jetstream.uri.teams.show', '/teams/{team}'), [TeamController::class, 'show'])->name('teams.show');
             Route::put('/teams/{team}', [TeamController::class, 'update'])->name('teams.update');
             Route::delete('/teams/{team}', [TeamController::class, 'destroy'])->name('teams.destroy');
             Route::put('/current-team', [CurrentTeamController::class, 'update'])->name('current-team.update');
